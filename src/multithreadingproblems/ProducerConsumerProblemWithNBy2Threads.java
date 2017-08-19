5e14ed257ded26746e2eca708ad2a9a22b4835de
Harsh <harshchiki@gmail.com>
2017-08-19 11:11:06 +0530

    More changes

---
+++ src/multithreadingproblems/ProducerConsumerProblemWithNBy2Threads.java
@@ -0,0 +1,12 @@
+package multithreadingproblems;
+
+/*
+ * Input n.
+ * No. of producer threads - n/2
+ * No. of consumer threads - n/2
+ * p1 produces 1, then c1 consumes 1, then p2 produces 2, then c2 consumes 2, and so on in this sequence.
+ * So basically, the last producer produces n/2
+ */
+public class ProducerConsumerProblemWithNBy2Threads {
+
+}
